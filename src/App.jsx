e869a6adec70c8c1f22bc9d6239450dfd6ed0c5b
Manu Singhal <manu.singhal2005@gmail.com>
2026-05-23 11:53:59 +0530

    full stack communication-express & frontend established

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
+
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5050"
+
 function App() {
-  return <h1>PrepPal</h1>
+  const [msg, setMsg] = useState("loading...")
+
+  useEffect(() => {
+    axios
+      .get(`${API_URL}/api/test`)
+      .then((res) => setMsg(res.data.message))
+      .catch(() => setMsg("could not reach server"))
+  }, [])
+
+  return <h1>{msg}</h1>
 }
 
 export default App
